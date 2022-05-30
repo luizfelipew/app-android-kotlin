cc2ea17f95cdeb1eca8d1478a52736e130f921c1
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2022-05-30 16:56:36 -0300

    configurando o viewBinding

--- GastoViagem/app/src/main/java/br/com/study/gastoviagem/MainActivity.kt
+++ GastoViagem/app/src/main/java/br/com/study/gastoviagem/MainActivity.kt
@@ -2,10 +2,18 @@ package br.com.study.gastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.study.gastoviagem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun calculate() {
     }
 }
